775292f41e256e1b0fd9f047507221aac5a498e5
wangshuo <>
2022-11-07 15:11:52 +0800

    feat(security): 安全模块
    1:安全模块初始化
    2:增加日志监控代码

--- wyci-utils/src/main/java/com/wyci/utils/aop/IncrementalLogAspect.java
+++ wyci-utils/src/main/java/com/wyci/utils/aop/IncrementalLogAspect.java
@@ -27,82 +27,83 @@ import java.util.UUID;
 @Component
 public class IncrementalLogAspect implements Serializable {
 
-  private static final Logger LOG = LoggerFactory.getLogger(IncrementalLogAspect.class);
+    private static final Logger LOG = LoggerFactory.getLogger(IncrementalLogAspect.class);
 
-  @Pointcut("execution(public * com.wyci.*(..))")
-  public void controllerPointcutLog() {}
-
-  @Before("controllerPointcutLog()")
-  public void doBeforeLog(JoinPoint joinPoint) {
-    // 增加日志流水号
-    MDC.put("LOG_ID", UUID.randomUUID().toString().replace("-", ""));
-    LOG.debug("------------- Log Start -------------");
-    beforeLogInfo(joinPoint);
-  }
-
-  @Around("controllerPointcutLog()")
-  public Object doAroundLog(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-    long startTime = System.currentTimeMillis();
-    Object result = proceedingJoinPoint.proceed();
-    doAroundLogInfo("返回结果: {}", result);
-    LOG.debug(
-        "------------- Log End 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
-    return result;
-  }
-
-  @After("controllerPointcutLog()")
-  public void afterLog() {
-    LOG.debug("============Log remove LOG_ID==================");
-    MDC.remove("LOG_ID");
-  }
-
-  /**
-   * 之前日志信息
-   *
-   * @param joinPoint 连接点
-   */
-  private void beforeLogInfo(JoinPoint joinPoint) {
-    // 开始打印请求日志
-    ServletRequestAttributes attributes =
-        (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-    if (null != attributes) {
-      HttpServletRequest request = attributes.getRequest();
-      LOG.debug("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
-      //            LOG.debug("远程地址: {}", request.getRemoteAddr());
+    @Pointcut("execution(public * com.wyci.*(..))")
+    public void controllerPointcutLog() {
     }
-    Signature signature = joinPoint.getSignature();
-    String name = signature.getName();
-    // 打印请求信息
-    LOG.debug("类名方法: {}.{}", signature.getDeclaringTypeName(), name);
 
-    // 打印请求参数
-    Object[] args = joinPoint.getArgs();
-
-    Object[] arguments = new Object[args.length];
-    for (int i = 0; i < args.length; i++) {
-      if (args[i] instanceof ServletRequest
-          || args[i] instanceof ServletResponse
-          || args[i] instanceof MultipartFile) {
-        continue;
-      }
-      arguments[i] = args[i];
+    @Before("controllerPointcutLog()")
+    public void doBeforeLog(JoinPoint joinPoint) {
+        // 增加日志流水号
+        MDC.put("LOG_ID", UUID.randomUUID().toString().replace("-", ""));
+        LOG.debug("------------- Log Start -------------");
+        beforeLogInfo(joinPoint);
     }
-    // 排除字段，敏感字段或太长的字段不显示
-    doAroundLogInfo("请求参数: {}", arguments);
-  }
 
-  /**
-   * 在日志信息
-   *
-   * @param format 格式
-   * @param result 结果
-   */
-  private void doAroundLogInfo(String format, Object result) {
-    // 排除字段，敏感字段或太长的字段不显示
-    String[] excludeProperties = {"password", "file"};
-    PropertyPreFilters filters = new PropertyPreFilters();
-    PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
-    excludefilter.addExcludes(excludeProperties);
-    LOG.debug(format, JSONObject.toJSONString(result, excludefilter));
-  }
+    @Around("controllerPointcutLog()")
+    public Object doAroundLog(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        doAroundLogInfo("返回结果: {}", result);
+        LOG.debug(
+                "------------- Log End 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+
+    @After("controllerPointcutLog()")
+    public void afterLog() {
+        LOG.debug("============Log remove LOG_ID==================");
+        MDC.remove("LOG_ID");
+    }
+
+    /**
+     * 之前日志信息
+     *
+     * @param joinPoint 连接点
+     */
+    private void beforeLogInfo(JoinPoint joinPoint) {
+        // 开始打印请求日志
+        ServletRequestAttributes attributes =
+                (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (null != attributes) {
+            HttpServletRequest request = attributes.getRequest();
+            LOG.debug("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+            //            LOG.debug("远程地址: {}", request.getRemoteAddr());
+        }
+        Signature signature = joinPoint.getSignature();
+        String name = signature.getName();
+        // 打印请求信息
+        LOG.debug("类名方法: {}.{}", signature.getDeclaringTypeName(), name);
+
+        // 打印请求参数
+        Object[] args = joinPoint.getArgs();
+
+        Object[] arguments = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                    || args[i] instanceof ServletResponse
+                    || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        // 排除字段，敏感字段或太长的字段不显示
+        doAroundLogInfo("请求参数: {}", arguments);
+    }
+
+    /**
+     * 在日志信息
+     *
+     * @param format 格式
+     * @param result 结果
+     */
+    private void doAroundLogInfo(String format, Object result) {
+        // 排除字段，敏感字段或太长的字段不显示
+        String[] excludeProperties = {"password", "file"};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.debug(format, JSONObject.toJSONString(result, excludefilter));
+    }
 }
