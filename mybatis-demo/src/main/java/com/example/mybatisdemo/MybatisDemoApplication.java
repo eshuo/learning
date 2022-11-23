208e797e7a1a071466b707e402f843591c4189f1
wangshuo <>
2022-11-23 09:24:22 +0800

    feat(mybatis): 使用MybatisSqlSessionFactoryBean
    1:使用MybatisSqlSessionFactoryBean

--- mybatis-demo/src/main/java/com/example/mybatisdemo/MybatisDemoApplication.java
+++ mybatis-demo/src/main/java/com/example/mybatisdemo/MybatisDemoApplication.java
@@ -1,8 +1,15 @@
 package com.example.mybatisdemo;
 
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @MapperScan("com.example.mybatisdemo.mapper")
@@ -12,4 +19,27 @@ public class MybatisDemoApplication {
         SpringApplication.run(MybatisDemoApplication.class, args);
     }
 
+
+    /**
+     * com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration#sqlSessionFactory类中的bean里面说明了创建SqlSessionFactory bean时
+     * ，自定义多数据源使用 MybatisSqlSessionFactoryBean 而不是 SqlSessionFactoryBean，否则无法调用mybatis的api，会如下错误
+     *
+     * @param dataSource
+     *
+     * @return
+     *
+     * @throws Exception
+     */
+    @Primary
+    @Bean("masterSqlSessionFactory")
+    public SqlSessionFactory masterSqlSessionFactory(DataSource dataSource) throws Exception {
+        /**
+         * 使用 mybatis plus 配置
+         */
+        MybatisSqlSessionFactoryBean b1 = new MybatisSqlSessionFactoryBean();
+//        System.out.println("dataSourceLyz"+dataSource.toString());
+        b1.setDataSource(dataSource);
+        b1.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/**/*.xml"));
+        return b1.getObject();
+    }
 }
