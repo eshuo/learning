e5f6251e29d5b366ce5c4ca6cfb103fe2c61228c
eshuo <eshuo@163.com>
2024-12-12 15:25:53 +0800

    feat(wx-demo): 添加微信图片转换工具
    - 新增 WxImgConvert 类，用于转换微信加密图片
    - 实现了图片异或解密和文件类型识别功能- 添加了多种文件类型的魔数映射
    - 通过多线程并行处理提高转换效率

---
+++ wx-demo/src/main/java/com/wyci/wxdemo/WxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wyci.wxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxDemoApplication.class, args);
+    }
+
+}
---
+++ wx-demo/src/test/java/com/wyci/wxdemo/WxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyci.wxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
