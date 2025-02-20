7d738c4bf25fbcd33294d3f45f91208f3a46fcf9
eshuo <eshuo@163.com>
2025-02-20 18:18:11 +0800

    feat(validate): 添加空值校验方法并优化代码格式
    - 新增 blank 方法用于验证空值并抛出异常
    - 优化代码格式，包括缩进、换行等
    - 删除多余注释和空行

--- wyci-utils/src/main/java/com/wyci/utils/validate/ValidateUtils.java
+++ wyci-utils/src/main/java/com/wyci/utils/validate/ValidateUtils.java
@@ -1,8 +1,8 @@
 package com.wyci.utils.validate;
 
 
-
 import java.util.regex.Pattern;
+import org.apache.commons.lang3.ObjectUtils;
 
 /**
  * @Description 验证工具类  如需动态验证 需要引入admin-feign服务
@@ -15,17 +15,16 @@ public class ValidateUtils {
 
     public static final String PHONE_PATTERN_STR = "^1(3\\d|4[5-9]|5[0-35-9]|6[567]|7[0-8]|8\\d|9[0-35-9])\\d{8}$";
 
-    public static final String ID_CARD_PATTERN_STR = "(^[1-9]\\d{5}(18|19|([23]\\d))\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$)|(^[1-9]\\d{5}\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}$)";
+    public static final String ID_CARD_PATTERN_STR = "(^[1-9]\\d{5}(18|19|([23]\\d))\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$)|(^[1-9]\\d{5}\\d{2}((0[1-9])|(10|11|12))("
+        + "([0-2][1-9])|10|20|30|31)\\d{3}$)";
 
     public static final String EMAIL_PATTERN_STR = "^[A-Za-z0-9\\u4e00-\\u9fa5]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$";
-
 
 
     /**
      * 验证手机号
      *
      * @param mobile
-     *
      * @return
      */
     public static boolean mobile(String mobile) {
@@ -37,7 +36,6 @@ public class ValidateUtils {
      * 验证身份证号
      *
      * @param idCard
-     *
      * @return
      */
     public static boolean idCard(String idCard) {
@@ -49,12 +47,24 @@ public class ValidateUtils {
      * 验证邮箱
      *
      * @param email
-     *
      * @return
      */
     public static boolean email(String email) {
         return Pattern.compile(EMAIL_PATTERN_STR).matcher(email).matches();
 
+    }
+
+
+    /**
+     * 验证空值抛出异常
+     *
+     * @param o       字符串
+     * @param message 消息
+     */
+    public static void blank(Object o, String message) {
+        if (ObjectUtils.isEmpty(o)) {
+            throw new RuntimeException(message);
+        }
     }
 
 }
